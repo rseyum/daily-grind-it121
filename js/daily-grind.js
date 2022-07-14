09fc9a70d9488d9be824e00d0285bfed7238a668
rseyum <103169939+rseyum@users.noreply.github.com>
2022-07-13 23:35:38 -0700

    Update daily-grind.js

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -18,13 +18,10 @@ let myDay = myDate.getDay();
 let today = "";
 let coffee = "";
 
-//use location object to access querystring (address bar)
 const queryString = window.location.search;
 
-//output to console
 console.log(queryString);
 
-//separate querystring parameters
 const urlParams = new URLSearchParams(queryString);
 
 if (urlParams.has("day")) {
